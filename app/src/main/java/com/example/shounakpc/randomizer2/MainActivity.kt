956bb03568fe15a115ab1d7abc2e7daf79deac5b
shounakdatta <shounak14datta@gmail.com>
2019-01-06 19:33:05 -0500

    First commit

---
+++ app/src/main/java/com/example/shounakpc/randomizer2/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.shounakpc.randomizer2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.view.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+        rollButton.setOnClickListener {
+            val rand = Random().nextInt(seekBar.progress)+1
+            resultsTextView.text = rand.toString()
+        }
+    }
+}
